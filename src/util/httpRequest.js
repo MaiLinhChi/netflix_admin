433ac772a504aff872e198e75f41a5dabdbdabfc
MaiLinhChi <mailinhchi22082002@gmail.com>
2023-12-02 16:24:30 +0700

    add env

--- src/util/httpRequest.js
+++ src/util/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: process.env.API_URL,
 });
 
 export const Get = async (url, options = {}) => {
